a22081a46c8bc2f774a0f6b2a538e40a385f2de1
lyg0114 <whdnseowkd@naver.com>
2023-03-25 22:42:29 +0900

    refactoring

--- src/main/java/com/suseong/ticketsuseongspring/TicketSuseongSpringApplication.java
+++ src/main/java/com/suseong/ticketsuseongspring/TicketSuseongSpringApplication.java
@@ -1,27 +1,21 @@
 package com.suseong.ticketsuseongspring;
 
+import com.suseong.ticketsuseongspring.conf.Global;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TicketSuseongSpringApplication {
 
   public static void main(String[] args) {
-    setUp();
-    ConfigurableApplicationContext run = SpringApplication
-        .run(TicketSuseongSpringApplication.class, args);
-    StartTicketProcess process = run.getBean(StartTicketProcess.class);
-    process.start();
-  }
-
-  private static void setUp() {
-    System.setProperty("webdriver.chrome.driver",
-        "/Users/iyeong-gyo/Desktop/study/toy-study/ticket-suseong/chromedriver");
+    System.setProperty(Global.DRIVER_NAME, Global.DRIVER_PATH);
+    SpringApplication.run(TicketSuseongSpringApplication.class, args)
+        .getBean(StartTicketProcess.class)
+        .start();
   }
 
   @Bean
---
+++ src/main/java/com/suseong/ticketsuseongspring/conf/Global.java
@@ -0,0 +1,11 @@
+package com.suseong.ticketsuseongspring.conf;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.suseong.ticketsuseongspring.conf
+ * @since : 2023/03/25
+ */
+public class Global {
+  public static final String DRIVER_NAME = "webdriver.chrome.driver";
+  public static final String DRIVER_PATH = "/Users/iyeong-gyo/Desktop/study/toy-study/ticket-suseong-spring/src/main/resources/chromedriver";
+}
