d53782a7ccf89ac111a0da1b606eb4e851c1c846
lyg0114 <whdnseowkd@naver.com>
2023-05-11 23:05:19 +0900

    Update : finish booking process

--- src/main/java/com/suseong/ticketsuseongspring/StartTicketProcess.java
+++ src/main/java/com/suseong/ticketsuseongspring/StartTicketProcess.java
@@ -4,7 +4,10 @@ import com.suseong.ticketsuseongspring.action.ActionLogin;
 import com.suseong.ticketsuseongspring.conf.GlobalVar;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -23,9 +26,31 @@ public class StartTicketProcess implements StartProcess {
   private final WebDriver driver;
   private final ActionLogin login;
 
+  private final String goSanBtnXPath = "/html/body/s/div[2]/div/div[1]/div/a/span";
+  private final String personalInfoCheckXPath = "/html/body/form/div[1]/div[2]/div/table/tbody/tr[15]/td/p/input";
+
   @Override
   public void start() {
     driver.get(global.getLoginUrl());
     login.startLogin();
+    goToYoGaPage();
+    checkPersonalInfo();
+  }
+
+  private void goToYoGaPage() {
+    WebElement goSanBtn = driver.findElement(By.xpath(goSanBtnXPath));
+    new Actions(driver)
+        .moveToElement(goSanBtn)
+        .click()
+        .perform();
+  }
+
+  private void checkPersonalInfo() {
+    driver.get(global.getTargetUrl());
+    WebElement personalInfoCheckBtn = driver.findElement(By.xpath(personalInfoCheckXPath));
+    new Actions(driver)
+        .moveToElement(personalInfoCheckBtn)
+        .click()
+        .perform();
   }
 }
